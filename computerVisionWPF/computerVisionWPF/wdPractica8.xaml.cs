471945dbe9e8ddb9e240cc44a1e31f59876ae4a1
BernardQG <b.quintinoguzman@ugto.mx>
2018-04-21 22:41:03 -0500

    Finalizacion de la practica
    Se sube la practica ya realizada y probada

--- computerVisionWPF/computerVisionWPF/wdPractica8.xaml.cs
+++ computerVisionWPF/computerVisionWPF/wdPractica8.xaml.cs
@@ -6,7 +6,13 @@ using Emgu.CV;
 using Emgu.CV.Structure;
 using System.Runtime.InteropServices;
 using Microsoft.Win32;
+using Emgu.CV.UI;
+using Emgu.CV.Util;
+using System.ComponentModel;
 
+using Emgu.CV.CvEnum;
+
+using System.Drawing;
 
 
 namespace computerVisionWPF
@@ -20,9 +26,9 @@ namespace computerVisionWPF
         Image<Gray, byte> imaO = new Image<Rgb, byte>(Environment.CurrentDirectory + @"\Imagenes\placa.jpg").Convert<Gray, byte>();
         Image<Gray, byte> imaAux = new Image<Rgb, byte>(Environment.CurrentDirectory + @"\Imagenes\placa.jpg").Convert<Gray, byte>();
         Image<Gray, byte> imaAux2 = new Image<Rgb, byte>(Environment.CurrentDirectory + @"\Imagenes\placa.jpg").Convert<Gray, byte>();
-        short[,] matIx;
-        short[,] matIy;
-        short[,] matD;//Guarda las direcciones
+        int[,] matIx;
+        int[,] matIy;
+        float[,] matD;//Guarda las direcciones
         int divMat = 0;
 
 
@@ -31,10 +37,10 @@ namespace computerVisionWPF
         public wdPractica8()
         {
             InitializeComponent();
-            //mascara = new int[,] { { 1, 4, 7, 4, 1 }, { 4, 16, 26, 16, 4 }, { 7, 26, 41, 26, 7 }, { 4, 16, 26, 16, 4 }, { 1, 4, 7, 4, 1 } };
-            //divMat = 273;
-            mascara = new int[,] { { 1, 2, 1 }, { 2, 4,2 }, {1, 2, 1}};
-            divMat = 16;
+            mascara = new int[,] { { 1, 4, 7, 4, 1 }, { 4, 16, 26, 16, 4 }, { 7, 26, 41, 26, 7 }, { 4, 16, 26, 16, 4 }, { 1, 4, 7, 4, 1 } };
+            divMat = 273;
+           // mascara = new int[,] { { 1, 2, 1 }, { 2, 4,2 }, {1, 2, 1}};
+            //divMat = 16;            
             def();
         }
         private void def() {
@@ -91,6 +97,7 @@ namespace computerVisionWPF
                 }
 
             }
+            CvInvoke.Imshow("Convolucion", imaAux);//Convolucion
             //ctlIma.Source = ToBitmapSource(imaAux);
 
         }
@@ -137,61 +144,44 @@ namespace computerVisionWPF
             }
         }
 
-        private void Canny() {
-            // 1. Convuluciona la imagen con un filtro gaussiano
-            Convolucion();
-            // 2. Encontral el gradiente de la imagen 
-            Ix(); Iy();
-            I(); Io();
-            // 3. Supresion de No maximos
-            discretisar();
-            comparacionVecinos();
-            // 4. Histeresis de doble umbral
-           histeresiDUmbral();
-        }
+       
 
 
         private void Ix()
         {
 
-            matIx = new short[imaAux.Height, imaAux.Width];
+            matIx = new int[imaAux.Height, imaAux.Width];
             for (int i = 0; i < imaAux.Height; i++)//recorre en vertical      
             {
                 for (int j = 0; j < imaAux.Width - 1; j++)//recorre en horizontal
                 {
-                    matIx[i, j] = (short)(imaAux.Data[i, j + 1, 0] - imaAux.Data[i, j, 0]);
+                    matIx[i, j] = (imaAux.Data[i, j + 1, 0] - imaAux.Data[i, j, 0]);
                 }
             }
 
             for (int i = 0; i < imaAux.Height; i++)//recorre en vertical      
             {
-                matIx[i, imaAux.Width - 1] = (short)(imaAux.Data[i, imaAux.Width - 1, 0]);
+                matIx[i, imaAux.Width - 1] = (imaAux.Data[i, imaAux.Width - 1, 0]);
             }
-
-            /*  for (int i = 0; i < imaAux.Height; i++)//recorre en vertical      
-              {
-                  for (int j = 0; j < imaAux.Width; j++)//recorre en horizontal
-                               imaAux.Data[i, j, 0] = (byte)matIx[i,j];
-              }
-              ctlIma.Source = ToBitmapSource(imaAux);//el gradiente*/
+         
         }
 
 
         private void Iy()
         {
 
-            matIy = new short[imaAux.Height, imaAux.Width];
+            matIy = new int[imaAux.Height, imaAux.Width];
             for (int i = 0; i < imaAux.Height - 1; i++)//recorre en vertical      
             {
                 for (int j = 0; j < imaAux.Width; j++)//recorre en horizontal
                 {
-                    matIy[i, j] = (short)(imaAux.Data[i + 1, j, 0] - imaAux.Data[i, j, 0]);
+                    matIy[i, j] = (imaAux.Data[i + 1, j, 0] - imaAux.Data[i, j, 0]);
                 }
             }
 
             for (int j = 0; j < imaAux.Width; j++)//recorre en horizontal
             {
-                matIy[imaAux.Height - 1, j] = (short)(imaAux.Data[imaAux.Height - 1, j, 0]);
+                matIy[imaAux.Height - 1, j] =(imaAux.Data[imaAux.Height - 1, j, 0]);
             }
 
            
@@ -209,57 +199,73 @@ namespace computerVisionWPF
                     imaAux.Data[i, j, 0] = (byte)(Math.Abs(Math.Sqrt(Math.Pow(matIx[i, j], 2) + Math.Pow(matIy[i, j], 2))));
                 }
             }
-
-          //   ctlIma.Source = ToBitmapSource(imaAux);//el gradiente
+            CvInvoke.Imshow("Gradiente", imaAux);//el gradiente               
         }
 
         private void Io()
         {
-            matD = new short[imaAux.Height, imaAux.Width];
+
+            System.Drawing.Bitmap b;
+            b = imaAux.Bitmap;            
+            Image<Gray, byte> iM = new Image<Gray, byte>(b);
+            
+            matD = new float[imaAux.Height, imaAux.Width];
 
             for (int i = 0; i < imaAux.Height; i++)//recorre en vertical      
             {
                 for (int j = 0; j < imaAux.Width; j++)//recorre en horizontal
                 {
                     if (matIx[i, j] != 0)
-                        matD[i, j] = (short)(Math.Tanh(matIy[i, j] / matIx[i, j]));
+                    
+                        matD[i, j] = (float)(Math.Atan(matIy[i, j]/ matIx[i, j])*(180/Math.PI));
+                    //Tangent = (float)(Math.Atan(DerivativeY[i, j] / DerivativeX[i, j]) * 180 / Math.PI); //rad to degree
                     else
-                        matD[i, j] = (short)(Math.Tanh(0));
+                        matD[i, j] = 90F;
 
+
+                    iM.Data[i, j, 0] =(byte)(matD[i, j]);
                 }
             }
+            CvInvoke.Imshow("Direeciones", iM);//el gradiente               
 
-          
         }
 
+        private void Canny()
+        {
+            // 1. Convuluciona la imagen con un filtro gaussiano
+            Convolucion();
+            // 2. Encontral el gradiente de la imagen 
+            Ix(); Iy();
+            I(); Io();
+            // 3. Supresion de No maximos
+            discretisar();
+            comparacionVecinos();
+            // 4. Histeresis de doble umbral
+            histeresiDUmbral(5,15);
+        }
 
         private void discretisar() {
 
             //discretizar matD
-            double vAux = 0;
+            float vAux = 0;
             for (int i = 0; i < matD.GetLength(0); i++)//recorre en vertical      
             {
                 for (int j = 0; j < matD.GetLength(1); j++)//recorre en horizontal
                 {
 
+                    vAux = matD[i, j];
+                    //Horizontal
+                    if ((vAux > -22.5 && vAux <= 22.5) || (vAux > 157.5 && vAux <= -157.5)) vAux = 0;
+                    //Vertical
+                    else if ((vAux > -112.5 && vAux <= -67.5)|| (vAux > 67.5 && vAux <= 112.5)) vAux = 1;
+                    //Diagonal +45
+                    else if ((vAux > -67.5 && vAux <= -22.5) || (vAux > 112.5 && vAux <= 157.5)) vAux = 2;
+                    //Diagonal -45
+                    else if ((vAux > -157.5 && vAux <= -112.5) || (vAux > 67.5 && vAux <= 22.5)) vAux = 3;
+                    
+                    
 
-                    if (matD[i, j] - 22.5 < 0) vAux = 360 - matD[i, j] - 22.5;
-                    else vAux = matD[i, j] - 22.5;
-
-                    if (vAux < 180) {
-                        if (vAux >= 0 && vAux < 45) vAux = 0;
-                        else if (vAux >= 45 && vAux < 90) vAux = 45;
-                        else if (vAux >= 90 && vAux < 135) vAux = 90;
-                        else vAux = 135;
-                    }
-                    else {
-                        if (vAux >= 180 && vAux < 225) vAux = 180;
-                        else if (vAux >= 225 && vAux < 270) vAux = 225;
-                        else if (vAux >= 270 && vAux < 315) vAux = 270;
-                        else vAux = 315;
-                    }
-
-                    matD[i, j] = (short)vAux;
+                    matD[i, j] = (float)vAux;
                 }
             }
 
@@ -276,84 +282,96 @@ namespace computerVisionWPF
             {
                 for (int j = 1; j < matD.GetLength(1) - 1; j++)//recorre en horizontal
                 {
-                    if (matD[i, j] == 0 || matD[i, j] == 180)
+                    //Horizontal
+                    if (matD[i, j] == 0 )
                     {
-                        if (imaAux.Data[i, j, 0] > imaAux.Data[i, j + 1, 0] && imaAux.Data[i, j, 0] > imaAux.Data[i, j - 1, 0])
-                            imaAux2.Data[i, j, 0] = imaAux.Data[i, j, 0];
-                        else imaAux2.Data[i, j, 0] = 0;
+                        if (imaAux.Data[i, j, 0] < imaAux.Data[i, j + 1, 0] || imaAux.Data[i, j, 0] < imaAux.Data[i, j - 1, 0])
+                            imaAux2.Data[i, j, 0] = 0;                        
+                    }
+                    //Vertical
+                    else if (matD[i, j] == 1)
+                    {
+                        if (imaAux.Data[i, j, 0] < imaAux.Data[i + 1, j, 0] || imaAux.Data[i, j, 0] < imaAux.Data[i - 1, j, 0])
+                            imaAux2.Data[i, j, 0] = 0;
+                       
+
+                    }
+                    //Diagonal +45
+                    else if (matD[i, j] == 2)
+                    {
+
+                        if (imaAux.Data[i, j, 0] < imaAux.Data[i - 1, j + 1, 0] || imaAux.Data[i, j, 0] < imaAux.Data[i + 1, j - 1, 0])
+                          imaAux2.Data[i, j, 0] = 0;
+
+                        
+                    }
+                    //Diagonal -45
+                    else
+                    {
+                        if (imaAux.Data[i, j, 0] < imaAux.Data[i - 1, j - 1, 0] || imaAux.Data[i, j, 0] < imaAux.Data[i + 1, j + 1, 0])
+                            imaAux2.Data[i, j, 0] = 0;
+
                     }
 
-                    else if (matD[i, j] == 90 || matD[i, j] == 270)
-                    {
-                        if (imaAux.Data[i, j, 0] > imaAux.Data[i + 1, j, 0] && imaAux.Data[i, j, 0] > imaAux.Data[i - 1, j, 0])
-                            imaAux2.Data[i, j, 0] = imaAux.Data[i, j, 0];
-                        else imaAux2.Data[i, j, 0] = 0;
-                    }
-                    else if (matD[i, j] == 135 || matD[i, j] == 312)
-                    {
-                        if (imaAux.Data[i, j, 0] > imaAux.Data[i - 1, j - 1, 0] && imaAux.Data[i, j, 0] > imaAux.Data[i + 1, j + 1, 0])
-                            imaAux2.Data[i, j, 0] = imaAux.Data[i, j, 0];
-                        else imaAux2.Data[i, j, 0] = 0;
-                    }
-
-                    else if (matD[i, j] == 45 || matD[i, j] == 225)
-                    {
-                        if (imaAux.Data[i, j, 0] > imaAux.Data[i - 1, j + 1, 0] && imaAux.Data[i, j, 0] > imaAux.Data[i + 1, j - 1, 0])
-                            imaAux2.Data[i, j, 0] = imaAux.Data[i, j, 0];
-                        else imaAux2.Data[i, j, 0] = 0;
-                    }
+                    
                 }
                 
 
             }
-            //ctlIma.Source = ToBitmapSource(imaAux2);
             
+            CvInvoke.Imshow("Supresion", imaAux2);//el gradiente       
+
 
         }
 
-        private void histeresiDUmbral()
-        {
-
-            Boolean bol = false;
-            int U1=20, U2=22;
-            for (int i = 1; i < matD.GetLength(0)-1; i++)//recorre en vertical      
+        private void histeresiDUmbral(int U1, int U2)
+        {                                 
+            for (int i = 0; i <imaAux2.Height; i++)//recorre en vertical      
             {
-                for (int j = 1; j < matD.GetLength(1)-1; j++)//recorre en horizontal
+                for (int j = 0; j < imaAux2.Width; j++)//recorre en horizontal
                 {
+                    if (imaAux2.Data[i, j, 0] >= U2)               
+                        imaAux2.Data[i, j, 0] = 255;
+                  
+                }
+            }
+            for(int m =0;m<100;m++)
+            UmbralEntreU1y2(U1, U2);
 
 
-                    if (imaAux2.Data[i, j, 0] > U2)
+            ctlIma.Source = ToBitmapSource(imaAux2);
+            
+        }
+        private void UmbralEntreU1y2(int U1, int U2) {
+
+            for (int i = 1; i < imaAux2.Height - 1; i++)//recorre en vertical      
+            {
+                for (int j = 1; j < imaAux2.Width- 1; j++)//recorre en horizontal
+                {
+                                                        
+                    if (imaAux2.Data[i, j, 0] >= U1 && imaAux2.Data[i, j, 0] < U2)
                     {
-                        imaAux2.Data[i, j, 0] = 0;
-                    }
-                    else if (imaAux2.Data[i, j, 0] > U1)
-                    {
-                        bol = false;
-                        for (int x = 0; x < mascara.GetLength(0); x++)
-                            for (int y = 0; y < mascara.GetLength(1); y++)
+                        
+                        for (int x = 0; x < 3; x++)
+                            for (int y = 0; y < 3; y++)
                             {
-
-                                if (imaO.Data[i + x - (int)(mascara.GetLength(0) / 2), j + y - (int)(mascara.GetLength(1) / 2), 0] > U2)
-                                    bol = true;
+                                
+                                if (imaAux2.Data[i + x - 1, j + y - 1, 0] == 255)
+                                    imaAux2.Data[i, j, 0] = 255;
 
 
                             }
-                        if (bol) imaAux2.Data[i, j, 0] = 0;
-                        else imaAux2.Data[i, j, 0] = 255;
-                    }
-                    else
-                    {
-                        imaAux2.Data[i, j, 0] = 255;
-                    }
-                    
-                    
-                    
+                       
+                    }                                  
                 }
+                
             }
-            ctlIma.Source = ToBitmapSource(imaAux2);
+            
+
+
         }
 
-       
+
         //end
     }
          
